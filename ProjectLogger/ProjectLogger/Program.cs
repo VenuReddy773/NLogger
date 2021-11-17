9625777753204fa224427e60ed36a43ec7748497
“VenuReddy773” <venu706@gmail.com>
2021-11-17 22:43:05 +0530

    [VenuGopalReddy] Added: NLogger Project

---
+++ ProjectLogger/ProjectLogger/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjectLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNumbers result = new AddNumbers();
+            result.Sum(2, 3);
+        }
+    }
+}
